d5cbc001d0c355f28118474de85b6819f0a8148f
Spandan Pokhrel <spandan.pokhrel2005@gmail.com>
2026-02-16 19:44:45 +0545

    Initial commit

---
+++ SSRF/ssrf.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"io"
+	_"net/url"
+)
+
+func main(){
+	http.HandleFunc("/", ssrf)
+	err:=http.ListenAndServe(":8080", nil)
+	if err!=nil{
+		fmt.Println(err)
+	}
+}
+
+func ssrf(w http.ResponseWriter, r *http.Request){
+	ssrf:=r.URL.Query().Get("ssrf")
+	if ssrf==""{
+		return
+	}
+	resp, err:=http.Get(ssrf)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	body, err:=io.ReadAll(resp.Body)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	io.WriteString(w, string(body))
+}
