db19a7934c3b1bdcb1119c33afa6ebdf40ceaa80
Spandan Pokhrel <spandan.pokhrel2005@gmail.com>
2026-02-18 23:09:05 +0545

    Adding New Case

---
+++ SSRF/Case III/ssrf.go
@@ -0,0 +1,46 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"io"
+	"net/url"
+	"strings"
+)
+
+func main(){
+	http.HandleFunc("/", ssrf)
+	err:=http.ListenAndServe(":8080", nil)
+	if err!=nil{
+		fmt.Println(err)
+	}
+}
+
+func ssrf(w http.ResponseWriter, r *http.Request){
+	ssrf:=r.URL.Query().Get("ssrf")
+	u, err :=url.Parse(ssrf)
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	hostname:=u.Hostname()
+	if ssrf==""{
+		return
+	}
+	if hostname=="vercel.app" || strings.HasSuffix(hostname, ".vercel.app"){
+		resp, err:=http.Get(ssrf)
+		if err!=nil{
+			fmt.Println(err)
+			return
+		}
+		body, err:=io.ReadAll(resp.Body)
+		if err!=nil{
+			fmt.Println(err)
+			return
+		}
+		io.WriteString(w, string(body))
+		return
+	}
+	w.WriteHeader(http.StatusForbidden)
+	io.WriteString(w, "Not Allowed!")
+}
