8470df2ea7a7dda68fc2b87e9b2d7413f88dd8fe
Spandan Pokhrel <spandan.pokhrel2005@gmail.com>
2026-02-21 20:13:30 +0545

    Creating Case IV

---
+++ SSRF/Case IV/ssrf.go
@@ -0,0 +1,52 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"io"
+	"net/url"
+	"strings"
+)
+
+func main(){
+	http.HandleFunc("/", ssrf)
+	err:=http.ListenAndServe(":8080", nil)
+	if err!=nil{
+		fmt.Println(err)
+	}
+}
+
+func ssrf(w http.ResponseWriter, r *http.Request){
+	ssrf:=r.URL.Query().Get("ssrf")
+	u, err :=url.Parse(ssrf)
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	hostname:=u.Hostname()
+	if ssrf==""{
+		return
+	}
+	if hostname=="vercel.app" || strings.HasSuffix(hostname, ".vercel.app"){
+		resp, err:=http.Get(ssrf)
+		if err!=nil{
+			fmt.Println(err)
+			return
+		}
+		finalURL:=resp.Request.URL
+		finalH := finalURL.Hostname()
+		if finalH=="vercel.app" || strings.HasSuffix(finalH, ".vercel.app"){
+			body, err:=io.ReadAll(resp.Body)
+			if err!=nil{
+				fmt.Println(err)
+				return
+			}
+			io.WriteString(w, string(body))
+			return
+		}
+		io.WriteString(w, "Final redirect doesn't match the white-list")
+		w.WriteHeader(http.StatusForbidden)
+	}
+	w.WriteHeader(http.StatusForbidden)
+	io.WriteString(w, "Not Allowed!")
+}
